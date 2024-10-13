cb441cc8d07aa18eb6291d2feb188f57785c1144
mustafasu499 <mhstafasu499@gmail.com>
2024-10-13 13:24:26 +0500

    Bhai Folder nhi a rah tha is waja se file de hai

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+      <View style={{marginVertical: 'auto', alignItems: 'center'}}>
+        <Text style={{fontSize: 30}}>Hello React Native</Text>
+      </View>
+
+  );
+};
+
+export default App;
